c8114f578b4be3308053d671e65c77eff90aeb98
Sayeed <sayeedulhoda@gmail.com>
2023-09-01 15:51:12 +0530

    make form boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Form from './Form.jsx';
+import ColorList from './ColorList.jsx';
+
+const App = () => {
+	return (
+		<main>
+			<Form />
+			<ColorList />
+		</main>
+	);
+};
+export default App;
