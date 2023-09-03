d8abda0be9aaca7fc9a5c6b73f7ce3b2fddc75b5
Sayeed <sayeedulhoda@gmail.com>
2023-09-03 17:06:18 +0530

    completed the project for color generator

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,18 @@ import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 const App = () => {
 	const [colors, setColors] = useState([new Values('#f15025').all(10)]);
+	const addColor = (color) => {
+		console.log(color);
+		try {
+			setColors([new Values(color).all(10)]);
+		} catch (error) {
+			console.log(error);
+			toast.error(error.message);
+		}
+	};
 	return (
 		<main>
-			<Form />
+			<Form addColor={addColor} />
 			<ColorList colors={colors[0]} />
 			<ToastContainer position="top-center" />
 		</main>
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,9 +1,22 @@
+import { toast } from 'react-toastify';
+
 const SingleColor = ({ index, color }) => {
 	const { hex, weight } = color;
+	const saveToClipborad = async () => {
+		if (navigator.clipboard) {
+			try {
+				await navigator.clipboard.writeText(`#${hex}`);
+				toast.success('color copied to clipborad');
+			} catch (error) {
+				toast.error('falied to copied the color');
+			}
+		}
+	};
 	return (
 		<article
-			className="color"
+			className={index > 10 ? 'color color-light' : 'color'}
 			style={{ background: `#${hex}` }}
+			onClick={saveToClipborad}
 		>
 			<p className="percent-value">{weight}%</p>
 			<p className="color-value">{hex}</p>
